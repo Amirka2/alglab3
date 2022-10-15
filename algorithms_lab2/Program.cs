07dea9b199a36b08495402a4d51c45efcc39b603
Amir <70907990+Amirka2@users.noreply.github.com>
2022-10-15 19:41:22 +0500

    list + standart methods

--- algorithms_lab2/Program.cs
+++ algorithms_lab2/Program.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using algorithms_lab2;
 
 public class Program
 {
     public static void Main(String[] args)
     {
-        Console.WriteLine('x');
+        int[] arr = { 1, 2, 3, 4, 5 };
+        MyList<int> l = new MyList<int>();
+        l.Add(1);
+        l.Add(2);
+        l.Add(3);
+        foreach(var e in l)
+            Console.WriteLine(e);
+
+        l.Remove(2);
+        Console.WriteLine();
+        foreach (var e in l)
+            Console.WriteLine(e);
+
+        l.AddFirst(0);
+        l.Add(4);
+        Console.WriteLine();
+        foreach (var e in l)
+            Console.WriteLine(e);
+
+        Console.ReadLine();
     }
 }
