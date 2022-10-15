1629f4ebd4aa955170312461513e524ede747124
Amir <70907990+Amirka2@users.noreply.github.com>
2022-10-15 19:43:31 +0500

    list + standart methods

---
+++ algorithms_lab2/ListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace algorithms_lab2
+{
+    public class ListItem<T>
+    {
+        public T Data { get; private set; }
+        public ListItem<T> Next { get; set; }
+        public ListItem<T> Prev { get; set; }
+
+        public ListItem(T data)
+        {
+            Data = data;
+        }
+    }
+}
+
